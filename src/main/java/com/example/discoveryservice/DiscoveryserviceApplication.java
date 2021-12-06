e76cc418e08ec1b718b1af4342a5a0384387ad55
Jeonghwan kim <43899546+conkjh032@users.noreply.github.com>
2021-12-06 22:49:18 +0900

    주석

--- src/main/java/com/example/discoveryservice/DiscoveryserviceApplication.java
+++ src/main/java/com/example/discoveryservice/DiscoveryserviceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
-@EnableEurekaServer // 서버로 등록하겠다.
+@SpringBootApplication // 이 어노테이션이 있는 파일을 실행하여 시작
+@EnableEurekaServer // 서버 역할로 지정
 public class DiscoveryserviceApplication {
 
     public static void main(String[] args) {
